f4e8d40d7f861c2c963be1ab522260b197cae7be
Amir Shekari Namini <2337117@csl-student.wlv.ac.uk>
2025-11-16 22:16:47 +0000

    Seperated the backend from the front end using twig templates engine

--- index.php
+++ index.php
@@ -1,17 +1,30 @@
 <?php
-	// Point to library
-	require_once 'vendor/autoload.php';
-	// Set up Environment
-	$loader = new \Twig\Loader\FilesystemLoader('.');
+
+	require_once __DIR__ . '/vendor/autoload.php';
+
+	// Connect to MySQL
+	$mysqli = new mysqli("localhost","2337117","Bioin150words","db2337117");
+
+	if ($mysqli->connect_errno) {
+    		die("Failed to connect: " . $mysqli->connect_error);
+	}
+
+	// Query movies
+	$query = "SELECT * FROM movies ORDER BY Movie_name"; 
+	$result = $mysqli->query($query);
+
+	// Convert result into array
+	$movies = [];
+	while ($row = $result->fetch_assoc()) {
+   		 $movies[] = $row;
+	}
+
+	// Setup Twig
+	$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
 	$twig = new \Twig\Environment($loader);
-	// Array of data
-	$people[0]['FirstName'] = "Alix";
-	$people[0]['Surname'] = "Bergeret";
-	$people[1]['FirstName'] = "Hiran";
-	$people[1]['Surname'] = "Patel";
-	// Load and render template
-	echo $twig->render('template.html',
-	array('a_variable' => 'Alix',
-	'another_variable' => 'Bergeret',
-	'people' => $people));
-?>
+
+	// Render Twig template
+	echo $twig->render('movies.twig', [
+    		'movies' => $movies
+	]);
+
