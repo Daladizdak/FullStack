267a57c6d5a4d7d0a6fa8d5bb72cba3f1e944897
Amir Shekari Namini <2337117@csl-student.wlv.ac.uk>
2025-11-20 16:53:03 +0000

    Using Ajax, now The website checks whether a movie already exists in the database or not. Also makes sure no field is empty

--- add_movie.php
+++ add_movie.php
@@ -7,43 +7,60 @@ include("db.php");
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
 $twig   = new \Twig\Environment($loader);
 
-$success = false;
+$error = '';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    $movieName   = $_POST['MovieName']   ?? '';
-    $genre       = $_POST['Genre']       ?? '';
-    $releaseDate = $_POST['ReleaseDate'] ?? '';
-    $score       = $_POST['Score']       ?? '';
+    $movieName   = trim($_POST['MovieName']   ?? '');
+    $genre       = trim($_POST['Genre']       ?? '');
+    $releaseDate = trim($_POST['ReleaseDate'] ?? '');
+    $score       = trim($_POST['Score']       ?? '');
 
-    // INSERT into films table
-    $sql = "INSERT INTO films (Movie_name, Genre, Release_Date, Score)
-            VALUES (?, ?, ?, ?)";
 
-    $stmt = mysqli_prepare($mysqli, $sql);
+    if ($movieName === '' || $genre === '' || $releaseDate === '' || $score === '') {
+        $error = 'All fields are required.';
+    } else {
+        $checkSql = "SELECT COUNT(*) AS cnt FROM films WHERE Movie_name = ?";
+        $checkStmt = mysqli_prepare($mysqli, $checkSql);
+        mysqli_stmt_bind_param($checkStmt, 's', $movieName);
+        mysqli_stmt_execute($checkStmt);
+        $checkResult = mysqli_stmt_get_result($checkStmt);
+        $row = $checkResult ? mysqli_fetch_assoc($checkResult) : null;
+        mysqli_stmt_close($checkStmt);
 
-    if ($stmt) {
-        mysqli_stmt_bind_param(
-            $stmt,
-            'sssi',  // s = string, i = integer
-            $movieName,
-            $genre,
-            $releaseDate,
-            $score
-        );
+        if ($row && (int)$row['cnt'] > 0) {
+            $error = 'This movie is already in the database.';
+        } else {
+            // INSERT into films table
+            $sql = "INSERT INTO films (Movie_name, Genre, Release_Date, Score)
+                    VALUES (?, ?, ?, ?)";
 
-        if (mysqli_stmt_execute($stmt)) {
+            $stmt = mysqli_prepare($mysqli, $sql);
 
-		header("Location: index.php?success=1");
-		exit;
+            if ($stmt) {
+                mysqli_stmt_bind_param(
+                    $stmt,
+                    'sssi',
+                    $movieName,
+                    $genre,
+                    $releaseDate,
+                    $score
+                );
 
+                if (mysqli_stmt_execute($stmt)) {
+                    header("Location: index.php?success=1");
+                    exit;
+                }
+
+                mysqli_stmt_close($stmt);
+            }
         }
-
-        mysqli_stmt_close($stmt);
     }
 }
 
-// Render the template
-echo $twig->render('add_movie.twig');
+// If not POST or there was an error, show the form
+echo $twig->render('add_movie.twig', [
+    'error' => $error
+]);
 
 mysqli_close($mysqli);
---
+++ check-movie.php
@@ -0,0 +1,30 @@
+<?php
+header('Content-Type: application/json');
+
+include("db.php");
+
+$movieName = isset($_POST['MovieName']) ? trim($_POST['MovieName']) : '';
+
+if ($movieName === '') {
+    echo json_encode(['exists' => false]);
+    exit;
+}
+
+$sql = "SELECT COUNT(*) AS cnt FROM films WHERE Movie_name = ?";
+$stmt = mysqli_prepare($mysqli, $sql);
+
+if (!$stmt) {
+    echo json_encode(['exists' => false]);
+    exit;
+}
+
+mysqli_stmt_bind_param($stmt, 's', $movieName);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+
+$row = $result ? mysqli_fetch_assoc($result) : null;
+$count = $row ? (int)$row['cnt'] : 0;
+
+mysqli_stmt_close($stmt);
+
+echo json_encode(['exists' => $count > 0]);
